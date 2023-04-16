554fba75a32a1b966b6fe47277511d53a71d275c
B4NGDAI <adnanrahmatulah@gmail.com>
2023-04-16 07:17:38 +0700

    add-firtst

---
+++ config.lua
@@ -0,0 +1,24 @@
+Config = Config or {}
+
+Config.Debug = false
+
+Config.Recipes = {
+
+    ["Meat"] = {
+        name =  'meat',
+        cooktime = 5000,
+        ingredients = {
+            [1] = { item = "raw_meat", amount = 1 }
+        },
+        receive = "cooked_meat"
+    },
+
+    ["Fish"] = {
+        name = 'fish',
+        cooktime = 5000,
+        ingredients = {
+            [1] = { item = "raw_fish", amount = 1 }
+        },
+        receive = "cooked_fish"
+    }
+}
